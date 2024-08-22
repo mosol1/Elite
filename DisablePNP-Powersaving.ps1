f76464e96beb69c4045376ae1d84838034e6ac4a
PRDGY_Aceツグ <88799791+mosol1@users.noreply.github.com>
2024-08-21 19:28:27 -0700

    Update DisablePNP-Powersaving.ps1

--- DisablePNP-Powersaving.ps1
+++ DisablePNP-Powersaving.ps1
@@ -1,5 +1,38 @@
-# Load the required assembly for Windows Forms
-Add-Type -AssemblyName System.Windows.Forms
+$hubs = Get-WmiObject Win32_USBController
+$powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
+foreach ($p in $powerMgmt) {
+    $IN = $p.InstanceName.ToUpper()
+    foreach ($h in $hubs){
+        $PNPDI = $h.PNPDeviceID
+        if ($IN -like "*$PNPDI*"){
+            $p.enable = $False
+            $p.psbase.put()
+        }
+    }
+}
 
-# Show the message box
-[System.Windows.Forms.MessageBox]::Show("Hello", "Message Box")
+$hubs = Get-WmiObject Win32_USBControllerDevice
+$powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
+foreach ($p in $powerMgmt) {
+    $IN = $p.InstanceName.ToUpper()
+    foreach ($h in $hubs){
+        $PNPDI = $h.PNPDeviceID
+        if ($IN -like "*$PNPDI*"){
+            $p.enable = $False
+            $p.psbase.put()
+        }
+    }
+}
+
+$hubs = Get-WmiObject Win32_USBHub
+$powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
+foreach ($p in $powerMgmt) {
+    $IN = $p.InstanceName.ToUpper()
+    foreach ($h in $hubs){
+        $PNPDI = $h.PNPDeviceID
+        if ($IN -like "*$PNPDI*"){
+            $p.enable = $False
+            $p.psbase.put()
+        }
+    }
+}
