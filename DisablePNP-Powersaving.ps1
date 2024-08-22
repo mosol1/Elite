10f2ca81acf78076b4dccd1659b61be008241e11
PRDGY_Aceツグ <88799791+mosol1@users.noreply.github.com>
2024-08-21 17:34:16 -0700

    Update DisablePNP-Powersaving.ps1

--- DisablePNP-Powersaving.ps1
+++ DisablePNP-Powersaving.ps1
@@ -1,38 +1,13 @@
-$hubs = Get-WmiObject Win32_USBController
-$powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
-foreach ($p in $powerMgmt) {
-    $IN = $p.InstanceName.ToUpper()
-    foreach ($h in $hubs){
-        $PNPDI = $h.PNPDeviceID
-        if ($IN -like "*$PNPDI*"){
-            $p.enable = $False
-            $p.psbase.put()
-        }
+# Define the MessageBox class
+Add-Type -TypeDefinition @"
+using System;
+using System.Windows.Forms;
+public class MessageBoxExample {
+    public static void ShowMessage() {
+        MessageBox.Show("Hello", "Message Box");
     }
 }
+"@
 
-$hubs = Get-WmiObject Win32_USBControllerDevice
-$powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
-foreach ($p in $powerMgmt) {
-    $IN = $p.InstanceName.ToUpper()
-    foreach ($h in $hubs){
-        $PNPDI = $h.PNPDeviceID
-        if ($IN -like "*$PNPDI*"){
-            $p.enable = $False
-            $p.psbase.put()
-        }
-    }
-}
-
-$hubs = Get-WmiObject Win32_USBHub
-$powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
-foreach ($p in $powerMgmt) {
-    $IN = $p.InstanceName.ToUpper()
-    foreach ($h in $hubs){
-        $PNPDI = $h.PNPDeviceID
-        if ($IN -like "*$PNPDI*"){
-            $p.enable = $False
-            $p.psbase.put()
-        }
-    }
-}
+# Call the ShowMessage method to display the message box
+[MessageBoxExample]::ShowMessage()
